2a5f0107fac2cb06346deeddf7bcaad58f0fefef
Naveed Zaheer <naveedzaheer@hotmail.com>
2020-07-17 00:40:30 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    var theDate = new Date(Date.now()).toLocaleString();
+    response.end("Hello Azure Power Lunch! - Today is: " + theDate);
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
